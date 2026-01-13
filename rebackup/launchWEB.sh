2b3eb268f7e0aeb67e7a5b9a43a3cf9e1ff3e05c
gibfredg13 <gibbons.wayne@gmail.com>
2026-01-13 11:52:57 +0100

    Update launchWEB.sh

--- rebackup/launchWEB.sh
+++ rebackup/launchWEB.sh
@@ -1,3 +1,3 @@
 cd ./docker && npm install
 cd ../vsftpd && npm install 
-npx concurrently "cd ../docker && npm run dev -- --host --port 3001" "cd ../vsftpd && npm run dev -- --host --port 3002"
+npx concurrently "cd ../docker && npm run dev -- --host --port 8066" "cd ../vsftpd && npm run dev -- --host --port 3002"
